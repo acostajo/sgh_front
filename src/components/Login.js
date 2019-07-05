53553a304b260dea9212f234aef93858fbd950df
acostajo <joacosta94@gmail.com>
2019-07-05 03:49:39 -0400

    se arreglo el login

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,8 @@ import * as actions from "../store/actions/auth";
 import { FlexboxGrid } from "rsuite";
 import logo from "./logolindo.png";
 import { Card, CardBody, CardImg } from "reactstrap";
+import axios from "axios";
+import { Alert } from "rsuite";
 
 const FormItem = Form.Item;
 const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
@@ -13,11 +15,40 @@ const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
 class NormalLoginForm extends React.Component {
   handleSubmit = e => {
     e.preventDefault();
-    this.props.form.validateFields((err, values) => {
+    this.props.form.validateFields(async (err, values) => {
       if (!err) {
-        this.props.onAuth(values.userName, values.password);
-        this.props.history.push("/menu/");
-        console.log(this.props.onAuth(values.userName, values.password));
+        var id_usuario;
+        await axios
+          .post("http://127.0.0.1:8000/authenticate/", {
+            username: values.userName,
+            password: values.password
+          })
+          .then(res => {
+            console.log(res.data);
+            id_usuario = res.data.id;
+          })
+          .catch(err => {
+            console.log(err);
+            Alert.warning("No se pudo iniciar sesión", 3000);
+          });
+
+        await axios
+          .get("http://127.0.0.1:8000/api/usuario/" + id_usuario)
+          .then(res => {
+            console.log(res.data);
+            const token = res.data.key;
+            const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
+            localStorage.setItem("token", token);
+            localStorage.setItem("expirationDate", expirationDate);
+            if (res.data.tipo === "medico") {
+              this.props.history.push("/menu/");
+            } else if (res.data.tipo === "lab") {
+              this.props.history.push("/menu_lab/");
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
       }
     });
   };
